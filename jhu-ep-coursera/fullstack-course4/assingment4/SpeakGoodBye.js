6aea8b174e96c42f68be46bdd1c09bfbc46adbdf
Jingang Miao <jingang_miao@apple.com>
2024-10-05 20:48:03 -0400

    add solution

--- jhu-ep-coursera/fullstack-course4/assingment4/SpeakGoodBye.js
+++ jhu-ep-coursera/fullstack-course4/assingment4/SpeakGoodBye.js
@@ -4,6 +4,14 @@
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
+(function (window) {
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -11,14 +19,10 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
--- jhu-ep-coursera/fullstack-course4/assingment4/script.js
+++ jhu-ep-coursera/fullstack-course4/assingment4/script.js
@@ -34,14 +34,24 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = [
+  "Yaakov",
+  "John",
+  "Jen",
+  "Jason",
+  "Paul",
+  "Frank",
+  "Larry",
+  "Paula",
+  "Laura",
+  "Jim",
+];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
+for (var i = 0; i < names.length; i++) {
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -49,16 +59,17 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+
+  if (firstLetter === "j") {
+    byeSpeaker.speak(names[i]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i]);
   }
 }
